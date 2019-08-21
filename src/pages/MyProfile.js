ada87a7ec7e73b0eaedc115c5e9fe16dd5b65310
Gloria Visus <gloriavisus@gmail.com>
2019-08-21 16:21:09 +0200

    myprofile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -6,14 +6,16 @@ import Navbar from "../components/Navbar";
 function MyProfile(props) {
   const {username, hobbies, image, description} = props.user;
   return (
-    <div className="miperfil">
-    <Navbar />
-     {image ? <img src={image} alt={username} /> : null}
-      <Link className='editar-perfil' to="/myprofile/edit">Editar perfil</Link>
-      <h3>{username}</h3>
-      <p>{hobbies}</p>
-      <p>{description}</p>
-    </div>
+    <>
+      <Navbar />
+        <div className="miperfil">
+        {image ? <img src={image} alt={username} /> : null}
+        <Link className='editar-perfil' to="/myprofile/edit">Editar perfil</Link>
+        <h3>{username}</h3>
+        <p>{hobbies}</p>
+        <p>{description}</p>
+        </div>
+    </>
   )
 }
 export default withAuth (MyProfile);
